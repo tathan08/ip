da1eb378892ce218d8a80bc4b072ceed4cd91363
tathan08 <tan.athan00@gmail.com>
2025-08-20 19:37:31 +0800

    Add user input echoing

--- src/main/java/Jinjja.java
+++ src/main/java/Jinjja.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jinjja {
     public static void printGreeting() {
         System.out.println("Hello! I'm Jinjja");
@@ -13,8 +15,26 @@ public class Jinjja {
     }
 
     public static void main(String[] args) {
+        // Greet user
         printDivider();
         printGreeting();
+        printDivider();
+        // Loop to handle user input
+        Scanner userInput = new Scanner(System.in);
+        boolean exitState = false;
+        while (!exitState) {
+            String command = userInput.nextLine();
+            switch(command) {
+                case "bye":
+                    exitState = true;
+                    break;
+                default:
+                    printDivider();
+                    System.out.println(command);
+                    printDivider();
+            }
+        }
+        userInput.close();
         printDivider();
         printFarewell();
         printDivider();
