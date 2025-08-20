f7c9f64fee72dbbefc128d77772b38414adcd44c
tathan08 <tan.athan00@gmail.com>
2025-08-20 17:47:03 +0800

    Update chatbot name from Duke to Jinjja and update greeting message to reflect this change

---
+++ src/main/java/Jinjja.java
@@ -0,0 +1,11 @@
+public class Jinjja {
+    public static void main(String[] args) {
+        String newline = "____________________________________________________________";
+        System.out.println(newline);
+        System.out.println("Hello! I'm Jinjja");
+        System.out.println("What can I do for you?");
+        System.out.println(newline);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(newline);
+    }
+}
