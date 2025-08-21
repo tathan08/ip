26cd5f68834cd75f4d0eebf232c4ae7480f20cd5
tathan08 <tan.athan00@gmail.com>
2025-08-21 21:06:15 +0800

    Add template for Deadline, Events, Todos tasks

---
+++ src/main/java/Todos.java
@@ -0,0 +1,10 @@
+class Todos extends Task {
+    public Todos(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
