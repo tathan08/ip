684d531ef620781ec92a45cde489cd93922bcf13
tathan08 <tan.athan00@gmail.com>
2025-09-03 01:04:42 +0800

    Add header comments for public methods for Tasks

--- src/main/java/jinjja/task/Deadline.java
+++ src/main/java/jinjja/task/Deadline.java
@@ -4,7 +4,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * Represents a deadline task with a specific date and time to be completed by.
+ * Represents a deadline task with a specific date and time to be completed by. This task type includes a deadline that
+ * indicates when the task should be finished.
  */
 public class Deadline extends Task {
     private static final DateTimeFormatter DATETIME_OUTPUT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
@@ -12,16 +13,34 @@ public class Deadline extends Task {
 
     private LocalDateTime by;
 
+    /**
+     * Constructs a Deadline task with the specified description and deadline.
+     *
+     * @param description The description of the deadline task
+     * @param by The date and time by which the task should be completed
+     */
     public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
+    /**
+     * Converts the deadline task to a format suitable for saving to a file. The format is "D | [completion status] |
+     * [description] | [deadline]".
+     *
+     * @return A string representation of the deadline task in file format
+     */
     @Override
     public String toFileFormat() {
         return "D | " + super.toFileFormat() + " | " + this.by.format(DATETIME_FILE);
     }
 
+    /**
+     * Returns a string representation of the deadline task for display. The format is "[D][completion status]
+     * [description] (by: [formatted deadline])".
+     *
+     * @return A string representation of the deadline task
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.by.format(DATETIME_OUTPUT) + ")";
--- src/main/java/jinjja/task/Task.java
+++ src/main/java/jinjja/task/Task.java
@@ -1,26 +1,48 @@
 package jinjja.task;
 
 /**
- * Represents a task with a description and completion status.
+ * Represents a task with a description and completion status. This is the base class for all task types in the Jinjja
+ * application.
  */
 public class Task {
     private boolean isDone;
     private String description;
 
+    /**
+     * Constructs a new Task with the specified description. The task is initially marked as not done.
+     *
+     * @param description The description of the task
+     */
     public Task(String description) {
         this.isDone = false;
         this.description = description;
     }
 
+    /**
+     * Sets the completion status of the task.
+     *
+     * @param isDone true if the task is completed, false otherwise
+     * @return true indicating the status was successfully changed
+     */
     public boolean setDone(boolean isDone) {
         this.isDone = isDone;
         return true; // Indicate that the status was changed
     }
 
+    /**
+     * Returns the completion status of the task.
+     *
+     * @return true if the task is completed, false otherwise
+     */
     public boolean getIsDone() {
         return this.isDone;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The task description
+     */
     public String getDescription() {
         return this.description;
     }
